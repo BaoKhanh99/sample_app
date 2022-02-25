4520d2756d5fd1df331b95aa47fd02433e5874e2
BaoKhanh99 <tlbk1999@gmail.com>
2022-02-25 10:30:05 +0700

    Chuong 7: SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/contact"
